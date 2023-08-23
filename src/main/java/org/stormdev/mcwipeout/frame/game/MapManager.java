3f0b73099a5b7968c46bc12620154c1122dac323
Stormbits <storm@stormdev.xyz>
2023-08-23 23:51:04 +0200

    - Shulker Prototyping + Testing, sideways works.

---
+++ src/main/java/org/stormdev/mcwipeout/frame/game/MapManager.java
@@ -0,0 +1,23 @@
+package org.stormdev.mcwipeout.frame.game;
+/*
+  Created by Stormbits at 2/12/2023
+*/
+
+import lombok.Getter;
+import org.stormdev.mcwipeout.Wipeout;
+
+import java.util.HashMap;
+
+public class MapManager {
+
+    private Wipeout plugin;
+
+    @Getter
+    private java.util.Map<String, Map> maps;
+
+    public MapManager(Wipeout plugin) {
+        this.plugin = plugin;
+        this.maps = new HashMap<>();
+
+    }
+}
